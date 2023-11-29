40e7b0b1d698a59c3b3971cff559d540f01eea87
Dmytro Shegel <dimashlegel@gmail.com>
2023-11-29 20:16:07 +0200

    added delete, put , post

--- src/homework/restAPI/restAPITest.js
+++ src/homework/restAPI/restAPITest.js
@@ -1,8 +1,49 @@
+import { useEffect, useState } from "react";
 import { useFetch } from "./hooks/useFetch";
+import axios from "axios";
+
+axios.defaults.baseURL = 'http://localhost:3030/';
 
 const RestApiTest = () => {
 
+	const [data, setData] = useState([]);
+	const [isPostLoading, setIsPostLoading] = useState(false);
 	const {data : contacts, isLoading, error} = useFetch('contacts');
+
+	useEffect(() => {
+		setData(contacts);
+	},[contacts])
+
+	const addContact = async () => {
+		setIsPostLoading(true);
+		const payload = {		
+			"name": "Solomia",
+			"lastName": "Shlegel",
+			"about": "from Kyiv"
+		}
+		const response = await axios.post('contacts', payload);
+		setData((prev) => [...prev, response.data]);
+		setIsPostLoading(false);
+	}
+
+	const deleteContact = async (id) => {
+		await axios.delete(`contacts/${id}`);
+		setData((prev) => prev.filter((item) => item.id !== id));
+	}
+	const editContact = async (id) => {
+		const payload = {		
+			"name": "Gerda",
+			"lastName": "Shlegel",
+			"about": "from Kyiv"
+		}
+		const response = await axios.put(`contacts/${id}`, payload);
+		setData((prev) => prev.map((item) => {
+			if (item.id === id) {
+				return response.data
+			}
+			return item;
+		}));
+	}
 
 	if (error) {
 		return <div>Something went wrong:<br />{error}</div>
@@ -11,8 +52,18 @@ const RestApiTest = () => {
 	return (<>
 		<h1>Phone Book</h1>
 		<ul>
-			{isLoading ? (<div>Loading...</div>) : (contacts.map((contact) => (<li key={contact.id}>{contact.name} {contact.lastName}</li>)))}
+			{isLoading ? (<div>Loading...</div>) : (data.map((contact) => (<li key={contact.id}>
+					<p>
+						{contact.name} {contact.lastName} 
+						<button type="button" onClick={() => deleteContact(contact.id)}>Delete</button>
+						<button type="button" onClick={() => editContact(contact.id)}>Edit</button>
+					</p>
+
+				</li>)))}
 		</ul>
+
+<button type="button" disabled={isPostLoading} onClick={addContact}>{isPostLoading ? 'Loading...' : 'Add new'}</button>
+
 	</>)
 }
 
